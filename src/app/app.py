c1801193fbc8636fdbe597e3db9afd531e5a42c4
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-10-23 04:33:38 +0300

    creating containers

--- src/app/app.py
+++ src/app/app.py
@@ -15,3 +15,28 @@ database = client.get_database_client(Config.COSMOS_DATABASE)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    
+    
+    
+# # Example function to create a container
+# def create_container(container_name):
+#     try:
+#         database.create_container(id=container_name, partition_key='/id', offer_throughput=400)
+#         print(f'Container {container_name} created.')
+#     except Exception as e:
+#         print(f'Error creating container: {e}')
+        
+        
+# # Function to create an item
+# def create_item(container_name, item):
+#     container = database.get_container_client(container_name)
+#     container.upsert_item(item)
+#     print(f'Item {item} created/updated in {container_name}.')
+
+# # Function to read an item
+# def read_item(container_name, item_id):
+#     container = database.get_container_client(container_name)
+#     item = container.read_item(item_id, partition_key=item_id)
+#     return item
